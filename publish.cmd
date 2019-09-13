4081049380992acb43fb770549bdffa57ddbe61d
Sam Eakin <sseakin@gmail.com>
2019-09-13 13:18:56 -0500

    base code - added a card link

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=spendassistant-Web-Deploy.pubxml -p:Password=az7HwuKlsd10BlmG3lj069Gr5bydlepykGpgp3HoLu3cmmKdeKA9YeKEzLJx
+
